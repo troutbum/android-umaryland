6a1f49eb9c07d217d69866b9488bc1c924c4533f
troutbum <genec2008@gmail.com>
2014-11-23 15:42:32 -0800

    Working through Android tutorial on camera

---
+++ DailySelfie/src/com/coursedroid/apps/photo/dailyselfie/MainActivity.java
@@ -0,0 +1,152 @@
+package com.coursedroid.apps.photo.dailyselfie;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private ImageView mImageView;
+
+	
+	
+	static final int REQUEST_TAKE_PHOTO = 1;
+
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            Toast.makeText(getApplicationContext(),
+	            		"Error trying to create image file",
+	            		Toast.LENGTH_LONG).show();;
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	        	// puts complete image into file system
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
+	    }
+	}
+	
+	// this method makes the photo available to "photo gallery"
+	private void galleryAddPic() {
+	    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+	    File f = new File(mCurrentPhotoPath);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
+	}
+	
+	// Resizes the taken photo to the ImageView
+	private void setPic() {
+	    // Get the dimensions of the View
+	    int targetW = mImageView.getWidth();
+	    int targetH = mImageView.getHeight();
+
+	    // Get the dimensions of the bitmap
+	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+	    bmOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    int photoW = bmOptions.outWidth;
+	    int photoH = bmOptions.outHeight;
+
+	    // Determine how much to scale down the image
+	    int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+	    // Decode the image file into a Bitmap sized to fill the View
+	    bmOptions.inJustDecodeBounds = false;
+	    bmOptions.inSampleSize = scaleFactor;
+	    bmOptions.inPurgeable = true;
+
+	    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    mImageView.setImageBitmap(bitmap);
+	}
+	
+//	// create Intent to take picture
+//	private void dispatchTakePictureIntent() {
+//	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//	    // checks to make sure Intent can be handled before starting
+//	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//	    }
+//	}
+	
+	// check usage of REQUEST_IMAGE_CAPTURE vs REQUEST_TAKE_PHOTO
+	// in this example.  preview vs final take?
+	
+	// result of Intent is returned as a bitmap
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        mImageView.setImageBitmap(imageBitmap);
+	    }
+	}
+	
+	// create unique file path to store camera image file
+	String mCurrentPhotoPath;
+	private File createImageFile() throws IOException {
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    // store in publicly, all apps can acccess
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
