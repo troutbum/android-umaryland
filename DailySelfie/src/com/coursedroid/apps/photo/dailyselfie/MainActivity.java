c65983900b9d413f729b71ad6e3a3690305e1f6e
Gene Cho <genec2008@gmail.com>
2014-11-23 21:07:07 -0800

    Debugging camera save to bitmap

--- DailySelfie/src/com/coursedroid/apps/photo/dailyselfie/MainActivity.java
+++ DailySelfie/src/com/coursedroid/apps/photo/dailyselfie/MainActivity.java
@@ -4,29 +4,36 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private static final String TAG = "DailySelfie";
 	private ImageView mImageView;
-
-	
-	
+	private Bitmap mImageBitmap;
+	String mCurrentPhotoPath;
 	static final int REQUEST_TAKE_PHOTO = 1;
 
+	// Create Intent to take picture
 	private void dispatchTakePictureIntent() {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    // Ensure that there's a camera activity to handle the intent
@@ -49,6 +56,15 @@ public class MainActivity extends Activity {
 	        }
 	    }
 	}
+	
+	// Setup button listener to take picture
+	Button.OnClickListener mTakePicOnClickListener = 
+			new Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			dispatchTakePictureIntent();
+		}
+	};
 	
 	// this method makes the photo available to "photo gallery"
 	private void galleryAddPic() {
@@ -82,9 +98,10 @@ public class MainActivity extends Activity {
 
 	    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
 	    mImageView.setImageBitmap(bitmap);
+	    mImageView.setVisibility(View.VISIBLE);
 	}
 	
-//	// create Intent to take picture
+//	// Create Intent to take picture
 //	private void dispatchTakePictureIntent() {
 //	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 //	    // checks to make sure Intent can be handled before starting
@@ -99,15 +116,26 @@ public class MainActivity extends Activity {
 	// result of Intent is returned as a bitmap
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	   Log.i(TAG, "entered onActivityResult");	   
+	   Log.i(TAG, "requestCode = " + requestCode + "resultCode = " + resultCode);
+		if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
 	        Bundle extras = data.getExtras();
+	        Log.i(TAG, "past extras = data.getExtras()");
 	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        Log.i(TAG, "past imageBitmap = extras.get(data)");
 	        mImageView.setImageBitmap(imageBitmap);
+	        Log.i(TAG, "past setImageBitmap");
 	    }
+	    
+//	    if (mCurrentPhotoPath != null) {
+//			setPic();
+//			galleryAddPic();
+//			mCurrentPhotoPath = null;
+//		}
+	    
 	}
 	
-	// create unique file path to store camera image file
-	String mCurrentPhotoPath;
+	// Create file to store image with unique file path using timestamp
 	private File createImageFile() throws IOException {
 	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 	    String imageFileName = "JPEG_" + timeStamp + "_";
@@ -125,10 +153,24 @@ public class MainActivity extends Activity {
 	    return image;
 	}
 	
+	
+	/** Called when the activity is first created. */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+		mImageBitmap = null;
+		
+		Button picBtn = (Button) findViewById(R.id.btnIntend);
+		setBtnListenerOrDisable( 
+				picBtn, 
+				mTakePicOnClickListener,
+				MediaStore.ACTION_IMAGE_CAPTURE
+		);
+		
+		
 	}
 
 	@Override
@@ -149,4 +191,41 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	/**
+	 * Indicates whether the specified action can be used as an intent. This
+	 * method queries the package manager for installed packages that can
+	 * respond to an intent with the specified action. If no suitable package is
+	 * found, this method returns false.
+	 * http://android-developers.blogspot.com/2009/01/can-i-use-this-intent.html
+	 *
+	 * @param context The application's environment.
+	 * @param action The Intent action to check for availability.
+	 *
+	 * @return True if an Intent with the specified action can be sent and
+	 *         responded to, false otherwise.
+	 */
+	public static boolean isIntentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list =
+			packageManager.queryIntentActivities(intent,
+					PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+
+	private void setBtnListenerOrDisable( 
+			Button btn, 
+			Button.OnClickListener onClickListener,
+			String intentName
+	) {
+		if (isIntentAvailable(this, intentName)) {
+			btn.setOnClickListener(onClickListener);        	
+		} else {
+			btn.setText( 
+				getText(R.string.cannot).toString() + " " + btn.getText());
+			btn.setClickable(false);
+		}
+	}
+	
 }
